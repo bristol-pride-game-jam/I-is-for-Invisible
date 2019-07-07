38d365837699ab6fc0e04fc4b9b8ebf9e6af5c71
Christopher Orchard <christopher.orchard@just-eat.com>
2019-07-07 11:22:30 +0100

    Added background

--- src/Person.gd
+++ src/Person.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 export var speed = 250
-export var gravity = 1300
+export var gravity = 100
 
 var screen_size
 func _ready():
@@ -9,7 +9,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = Vector2()
+	if (Input.is_action_pressed("ui_right")):
+		velocity.x += 1
+	if (velocity.length() > 0):
+		velocity = velocity.normalized() * speed
+#		add walking animation
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
 	
 func _physics_process(delta):
 	var velocity = Vector2()
@@ -18,4 +25,4 @@ func _physics_process(delta):
 	
 	if (!is_on_floor()):
 		velocity.y += gravity * delta
-	move_and_collide(Vector2(0,1))
+	move_and_collide(velocity)
